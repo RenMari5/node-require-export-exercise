603f5cd4d8c3e9721d90feae24c679eaa1f3a295
Ren <rpaczkowski@gmail.com>
2022-06-13 19:12:35 -0400

    Fixed the cowsay error

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const cowsay = require("cowsay");
+
+const _ = require("lodash");
+
 const information = require("./data.js");
 
 const math = require("./sum.js");
-
-// const cowsay = require("cowsay");
-
-const shuffle = require("lodash.shuffle");
 
 console.log(information.numbers);
 console.log(`${information.name} is from ${information.hometown}`);
@@ -14,3 +14,10 @@ math(information.numbers);
 let shuffledNumbers = _.shuffle(information.numbers);
 
 console.log(shuffledNumbers);
+
+console.log(
+  cowsay.say({
+    text: "Whaaaaaaat?!",
+    e: "OO",
+  })
+);
