a14dcb8b97c0c34a26848f12d401606d573d93cd
Ren <rpaczkowski@gmail.com>
2022-06-13 09:40:06 -0400

    Added requires to index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const information = require("./data.js");
+const numbers = require("./data.js");
+const namae = require("./data.js");
+const whereImFrom = require("./data.js");
+
+const math = require("./sum.js");
+
+console.log(numbers());
+console.log(`${namae} is from ${whereImFrom}.`);
+
+module.exports = { numbers, namae, whereImFrom };
