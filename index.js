10ed0ab7a5499c767df3acccb0bf8aa6c8e7541a
Ren <rpaczkowski@gmail.com>
2022-06-13 14:20:09 -0400

    Working on lodash portion of task 2

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const information = require("./data.js");
 
 const math = require("./sum.js");
 
-const cowsay = require("cowsay");
+// const cowsay = require("cowsay");
+
+const lodash = require("lodash/array");
 
 console.log(information.numbers);
 console.log(`${information.name} is from ${information.hometown}`);
 
 math(information.numbers);
+
+console.log(information.numbers.arrayShuffle);
